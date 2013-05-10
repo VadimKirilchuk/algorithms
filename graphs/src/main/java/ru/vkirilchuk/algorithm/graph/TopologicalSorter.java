2d6c2321c2ad727dda05386a0ed414b754cf09b3
vkirilchuk <vkirilchuk@griddynamics.com>
2013-05-11 00:16:25 +0400

    initial commit with topological sort

---
+++ graphs/src/main/java/ru/vkirilchuk/algorithm/graph/TopologicalSorter.java
@@ -0,0 +1,53 @@
+package ru.vkirilchuk.algorithm.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+//TODO: javadoc with links
+//TODO: input and output should be parametrized by generics
+//TODO: add different algorithms, current is depth-first-search
+//TODO: create common graph representations (adjacent matrix, adjacent vectors,etc)
+public class TopologicalSorter {
+
+    private static enum Mark {
+        NOT_VISITED, VISITED, EXHAUSTED
+    }
+
+    private final int             size;
+    private final List<Integer>[] graph;
+    private final Mark[]          marks;
+
+    public TopologicalSorter(List<Integer>[] graph) {
+        this.size = graph.length;
+        this.graph = graph;
+        this.marks = new Mark[size];
+        Arrays.fill(marks, Mark.NOT_VISITED);
+    }
+
+    public List<Integer> sort() {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int index = 0; index < size; index++) {
+            if (marks[index] == Mark.NOT_VISITED) {
+                depthFirstSearch(result, index);
+            }
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+    private void depthFirstSearch(List<Integer> result, int index) {
+        marks[index] = Mark.VISITED;
+        for (int adjacentIndex : graph[index]) {
+            if (marks[adjacentIndex] == Mark.VISITED) { // cycle detected
+                throw new RuntimeException("Graph must have no cycles. Cycle detected at element: " + adjacentIndex);
+            }
+            if (marks[adjacentIndex] == Mark.NOT_VISITED) {
+                depthFirstSearch(result, adjacentIndex);
+            }
+        }
+        marks[index] = Mark.EXHAUSTED;
+        result.add(index);
+    }
+}
---
+++ graphs/src/test/java/ru/vkirilchuk/algorithm/graph/TopologicalSorterTest.java
@@ -0,0 +1,35 @@
+package ru.vkirilchuk.algorithm.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.carrotsearch.randomizedtesting.RandomizedTest;
+
+//TODO: more tests
+public class TopologicalSorterTest extends RandomizedTest {
+
+    private TopologicalSorter underTest;
+
+    @Test //TODO: can it be non reversed?
+    public void testOrderReversedForNotDependentElements() {
+        int size = atLeast(3);
+        
+        @SuppressWarnings("unchecked")
+        List<Integer>[] graph = new List[size];
+        
+        for (int index = 0; index < size; ++index) {
+            graph[index] = new ArrayList<Integer>();
+        }
+
+        underTest = new TopologicalSorter(graph);
+        List<Integer> result = underTest.sort();
+        assertEquals("Result preserves number of elements in graph", graph.length, result.size());
+
+        for (int index = 0; index < size; ++index) {
+            assertEquals("Result values are descending integers", size - index - 1, (int) result.get(index));
+        }
+    }
+
+}
